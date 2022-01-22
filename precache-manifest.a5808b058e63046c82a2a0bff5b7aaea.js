5ae998fb5a226f02204e7b8837217294c2838351
Zhuchara32 <vovazhuchara@gmail.com>
2022-01-22 18:02:21 +0200

    deploy

---
+++ precache-manifest.a5808b058e63046c82a2a0bff5b7aaea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dade33f5250b95a20a2f",
+    "url": "/list-order/css/app.7dce61ed.css"
+  },
+  {
+    "revision": "d20c216ddd58521b5d5f",
+    "url": "/list-order/css/chunk-vendors.46e3fa1b.css"
+  },
+  {
+    "revision": "5b3232c5ff0816af2eaf65db032fdb55",
+    "url": "/list-order/index.html"
+  },
+  {
+    "revision": "dade33f5250b95a20a2f",
+    "url": "/list-order/js/app.205341ff.js"
+  },
+  {
+    "revision": "da495da4cb7fd4c59644",
+    "url": "/list-order/js/chunk-2d0d36a4.a10ca460.js"
+  },
+  {
+    "revision": "d20c216ddd58521b5d5f",
+    "url": "/list-order/js/chunk-vendors.db34b50c.js"
+  },
+  {
+    "revision": "c76dc794f0a90973ed2240c58944c16a",
+    "url": "/list-order/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/list-order/robots.txt"
+  }
+]);
